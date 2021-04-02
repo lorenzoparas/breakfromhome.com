ff92b7e33519fd70a152d70abf7da1f82eb1e6d8
surga <surga3.carlos@gmail.com>
2021-04-03 01:14:24 +1100

    hello

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -2,10 +2,32 @@ import React from 'react';
 
 import './Home.css';
 
+
+
 function Home () {
     return (
         <div id="mainPage">
-            <div id="text">This is the home page</div>
+            <div id="text">
+                <h1>Scheduling website</h1>
+            </div> 
+            <div>
+                Sign up
+                <div>
+                <a href="/signUp">
+                    <button> Sign Up </button>
+                </a>
+                </div>
+            </div>
+            <div>
+                <a href="/preferences"> 
+                    <button> Preferences </button>
+                </a>
+            </div>
+            <div>
+                <a href="/time">
+                    <button> Time </button>
+                </a>
+            </div>
         </div>
     );
 }
