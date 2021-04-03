d87a1edce233c35310fffa4c7818fcb93eb9f674
surga <surga3.carlos@gmail.com>
2021-04-04 02:13:50 +1100

    Homepage functionality fixed

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/BitsGiggles.png';
 
 import useStyles from './styles';
 
 function Home () {
     const classes = useStyles();
     return (
+        // Main webpage
         <div id="mainPage" class={classes.mainPage}>
+        <div id={classes.navBar}>
+            <Link>
+                <img id={classes.logo} src={logo}/>
+            </Link>
+            <Link id={classes.rego} to='/signUp'>Register</Link>
+            <Link id={classes.login} to='/login'>Login</Link>
+        </div>
         <div id="text" class={classes.text}>
-            <h1>Scheduling website</h1>
+            <h1>Maximising time for our customers busy lives</h1>
         </div> 
+
+        {/* Content */}
         <div>
-            <a href="/signUp">
-                <button> Sign Up </button>
+            <h3>For students</h3>
+            <div>
+                Join our program to get hold of your busy life and enjoy your free time whilst maintaining your habits.
+            </div>
+            <a href='/signUp'>
+                <button>Register now</button>
             </a>
         </div>
+
+        {/* Footnotes */}
         <div>
-            <a href="/login">
-                <button> Login </button>
-            </a>
-        </div>
-        <div>
-            <a href="/websitePreferences"> 
-                <button> Preferences </button>
-            </a>
-        </div>
-        <div>
-            <a href="/timePreferences">
-                <button> Time </button>
-            </a>
         </div>
     </div>
     );
