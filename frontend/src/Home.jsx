06914d7604abeba5f8ca2deeb52faca91dfcbd36
fives <ivesparas@gmail.com>
2021-04-03 20:00:31 +1100

    stuff

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -7,6 +7,7 @@ import './Home.css';
 function Home () {
     return (
         <div id="mainPage">
+<<<<<<< HEAD
             <div id="text">
                 <h1>Scheduling website</h1>
             </div> 
@@ -28,7 +29,32 @@ function Home () {
                     <button> Time </button>
                 </a>
             </div>
+=======
+        <div id="text">
+            <h1>Scheduling website</h1>
+        </div> 
+        <div>
+            <a href="/signUp">
+                <button> Sign Up </button>
+            </a>
+>>>>>>> frontend_production
         </div>
+        <div>
+            <a href="/login">
+                <button> Login </button>
+            </a>
+        </div>
+        <div>
+            <a href="/preferences"> 
+                <button> Preferences </button>
+            </a>
+        </div>
+        <div>
+            <a href="/time">
+                <button> Time </button>
+            </a>
+        </div>
+    </div>
     );
 }
 
