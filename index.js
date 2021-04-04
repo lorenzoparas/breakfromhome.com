0e54f180ff2b2c82d5d006b2f75a2a2c7bb80794
Nathan <nathanbaylon@gmail.com>
2021-04-05 05:41:30 +1000

    Changing to heroku directories

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import path from 'path';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
@@ -15,6 +16,15 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
+
+// static
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 const CONNECTION_URL = 'mongodb+srv://bitsngiggles:bitsngiggles123@cluster0.u16nm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
