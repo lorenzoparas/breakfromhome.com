b56324e25b4f6d261ffe80049a6c0389e953902d
aryan <karwalaryan@gmail.com>
2021-04-05 02:52:44 +1000

    merging

--- frontend/src/pages/Pomodoro/Pomodoro.js
+++ frontend/src/pages/Pomodoro/Pomodoro.js
@@ -1,26 +1,13 @@
 import React, { useState } from 'react';
 import { Paper, Typography, Grid } from '@material-ui/core';
 import Timer from '../../components/Timer/Timer';
-<<<<<<< HEAD
 import NavBar from '../../components/NavBarLogin/NavBarLogin';
-=======
 import Navigation from '../../components/Navigation/Navigation';
->>>>>>> a32d5ba5d5209a2ba922353773a2c884a551139f
 import useStyles from '../Pomodoro/style';
 
 const Pomodoro = () => {
 
     const classes = useStyles();
-<<<<<<< HEAD
-    console.log(sessionStorage.getItem('loggedInUser'));
-    return (
-        <React.Fragment>
-        <NavBar/>
-        <Grid container align="center" justify="center" direction="column" className={classes.fix}>
-            <Typography variant="h1" color="primary">Pomodoro Timer</Typography>
-            <Timer/>
-        </Grid>
-=======
 
     return (
         <React.Fragment>
@@ -31,7 +18,6 @@ const Pomodoro = () => {
                 <Timer/>
             </Paper>
             </Grid>
->>>>>>> a32d5ba5d5209a2ba922353773a2c884a551139f
         </React.Fragment>
     );
 };
