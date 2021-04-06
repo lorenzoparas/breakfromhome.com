ccf0712a85dfae3f9817702ff184f0434ecac6ec
Nathan <nathanbaylon@gmail.com>
2021-04-07 02:37:12 +1000

    Changed client to frontend in index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/users', userRoutes);
 // static
 if(process.env.NODE_ENV === 'production') {
   // set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('frontend/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
