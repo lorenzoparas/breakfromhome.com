eb2c6a1bba9aae7513b90f957cca3f216c1a3403
Nathan <nathanbaylon@gmail.com>
2021-04-21 17:30:17 +1000

    Added config files'

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { fileURLToPath } from 'url';
 import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 
-const db = require('./config/keys').mongoURI;
+ import db from './config/keys';
 
 dotenv.config();
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -33,7 +33,7 @@ if(process.env.NODE_ENV === 'production') {
   });
 }
 
-const CONNECTION_URL =  db || process.env.MONGODB_URI;
+const CONNECTION_URL =  db.mongoURI || process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
